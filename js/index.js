c823b47bfcd3e8773e77d7ef78789fe9ec3553c0
Mugdhashrivastava <84615010+Mugdhashrivastava@users.noreply.github.com>
2022-05-06 20:05:29 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 
 
 let category = document.getElementsByClassName('category');
+$(document).ready(function() {
+    
+    var audio  = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3');
+    var audio2 = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3')
+  
+    $("#navResourceBtn").mousedown(function() {
+      audio2.load();
+      audio2.play();
+    });
 
 const selectBtn = document.querySelectorAll(".btn-style");
 const contributionBtn = document.querySelector(".contribution-btn");
